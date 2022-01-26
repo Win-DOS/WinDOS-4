817f1aadcb635325f34c7d883b42a89a6e56f531
Le Trieu Duong <duong032589@stu.vinschool.edu.vn>
2022-01-26 16:28:14 +0700

    Update selfupd.bat

--- WinDOS 4.2/selfupd.bat
+++ WinDOS 4.2/selfupd.bat
@@ -1,21 +1,21 @@
 @echo off
 color 01
-title WinDOS 3.4 Updater
+title WinDOS Updater
 echo Checking Manufacter's version's signature...
-IF EXIST "YourDisk\Path\version4.signature". msg * You are up-to-date!. ELSE goto DownUpdate.
+IF EXIST "C:\Users\%username%\WinDOS 4.2\version5.signature" msg * You are up-to-date! ELSE goto DownUpdate.
 :DownUpdate
 cls
 set /p Location=Type where you want the WinDOS folder to placed eg. [:drive][path]
 echo Downloading update...
-powershell -Command "Invoke-WebRequest https://github.com/TartSoft/WinDOS-3.4-main.zip -OutFile WinDOS-3.4-main.zip"
+powershell -Command "Invoke-WebRequest https://github.com/Win-DOS/WinDOS-Update-Repo/WinDOS-4.2.zip -OutFile WinDOS-4.2.zip"
 timeout 5 >NUL
 echo.
 echo Unzip files...
-unzip.exe WinDOS-3.4-main.zip 
+unzip.exe WinDOS-4.2.zip 
 timeout 5 >NUL
 echo.
 echo Move files to location...
-MOVE /Y Downloads/WinDOS-3.4-main %Location%
+MOVE /Y C:\Users\%username%\Downloads\WinDOS-4.2 %Location%
 timeout 5 >NUL
 echo.
 echo Finish!
